1b4be856798d9306c1ba0a0b15e5e5e228be56d2
Sriram-0305 <sriramshivanadhuni@gmail.com>
2026-04-15 14:20:22 +0530

    fixed API base URL in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an axios instance with a default base URL
 // We use a placeholder local URL as requested
 const api = axios.create({
-  baseURL: 'https://resume-builder-c8eg.onrender.com',
+  baseURL: 'https://resume-builder-c8eg.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
